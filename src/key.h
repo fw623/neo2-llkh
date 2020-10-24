c92eab39e0d96e0dda500c6cff8a4da1e7a0778a
fw623 <& 'c:\Users\maxim\.vscode\extensions\ms-vscode.cpptools-0.29.0\debugAdapters\bin\WindowsDebugLauncher.exe' '--stdin=Microsoft-MIEngine-In-jvfj1bpq.ax4' '--stdout=Microsoft-MIEngine-Out-b55ibyye.okd' '--stderr=Microsoft-MIEngine-Error-qwkfu5ok.fzg' '--pid=Microsoft-MIEngine-Pid-jxieu1et.wuw' '--dbgExe=C:\MinGW\bin\gdb.exe' '--interpreter=mi' @.com>
2020-10-25 00:37:00 +0200

    added ScanCode header

---
+++ src/key.h
@@ -0,0 +1,74 @@
+#ifndef KEY_H_
+#define KEY_H_
+
+// other
+#define SCAN_ESC 1
+#define SCAN_BACKSPACE 14
+#define SCAN_TAB 15
+#define SCAN_RETURN 28
+#define SCAN_CAPS 58
+#define SCAN_LSHIFT 42
+#define SCAN_RSHIFT 54
+#define SCAN_CTRL 29 // note: right and left are same
+#define SCAN_ALT 56 // note: right and left are same
+#define SCAN_LWIN 91
+#define SCAN_SPACE 57
+#define SCAN_RALT_CTRL 541 // altGr sends alt+lctrl (with different scancode)
+
+// numrow
+#define SCAN_Hat 41
+#define SCAN_1 2
+#define SCAN_2 3
+#define SCAN_3 4
+#define SCAN_4 5
+#define SCAN_5 6
+#define SCAN_6 7
+#define SCAN_7 8
+#define SCAN_8 9
+#define SCAN_9 10
+#define SCAN_0 11
+#define SCAN_SSharp 12
+#define SCAN_Apo 13
+
+// top row
+#define SCAN_Q 16
+#define SCAN_W 17
+#define SCAN_E 18
+#define SCAN_R 19
+#define SCAN_T 20
+#define SCAN_Z 21
+#define SCAN_U 22
+#define SCAN_I 23
+#define SCAN_O 24
+#define SCAN_P 25
+#define SCAN_Ue 26
+#define SCAN_Plus 27
+
+// home row
+#define SCAN_A 17
+#define SCAN_S 18
+#define SCAN_D 19
+#define SCAN_F 20
+#define SCAN_G 21
+#define SCAN_H 22
+#define SCAN_J 23
+#define SCAN_K 24
+#define SCAN_L 25
+#define SCAN_Oe 26
+#define SCAN_Ae 27
+#define SCAN_Hash 43
+
+// bot row
+#define SCAN_Less 86
+#define SCAN_Y 44
+#define SCAN_X 45
+#define SCAN_C 46
+#define SCAN_V 47
+#define SCAN_B 48
+#define SCAN_N 49
+#define SCAN_M 50
+#define SCAN_Comma 51
+#define SCAN_Dot 52
+#define SCAN_Dash 53
+
+#endif /* KEY_H_ */
