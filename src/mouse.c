de35c0eeaec50aa5bef8782bb012e090551f80c8
fw623 <& 'c:\Users\maxim\.vscode\extensions\ms-vscode.cpptools-0.29.0\debugAdapters\bin\WindowsDebugLauncher.exe' '--stdin=Microsoft-MIEngine-In-jvfj1bpq.ax4' '--stdout=Microsoft-MIEngine-Out-b55ibyye.okd' '--stderr=Microsoft-MIEngine-Error-qwkfu5ok.fzg' '--pid=Microsoft-MIEngine-Pid-jxieu1et.wuw' '--dbgExe=C:\MinGW\bin\gdb.exe' '--interpreter=mi' @.com>
2020-10-25 00:04:13 +0200

    readded shift to slow down movement

--- src/mouse.c
+++ src/mouse.c
@@ -4,64 +4,71 @@ typedef struct NavigationMapping {
   DWORD scan;
   int period; // 0 ... only run once
   int value;
-  void (*timerEvent)(struct NavigationMapping *key); // gets attached to timer
-  void (*releaseEvent)(struct NavigationMapping *key); // gets called on key release
+  int shiftedValue; // value when shift is active
+  void (*timerEvent)(const struct NavigationMapping *key); // gets attached to timer
+  void (*releaseEvent)(const struct NavigationMapping *key); // gets called on key release
   HANDLE timer;
   struct NavigationMapping *n;
 } NavigationMapping;
 
-void moveUp(NavigationMapping *key) {
-  mouse_event(MOUSEEVENTF_MOVE, 0, -key->value, 0, 0);
+int getValue(const NavigationMapping *key) {
+  volatile NeoModState *shift = &modStates.shift;
+  bool shiftIsActive = (shift->leftIsPressed || shift->rightIsPressed) != shift->isLocked;
+  return shiftIsActive ? key->shiftedValue : key->value;
 }
 
-void moveLeft(NavigationMapping *key) {
-  mouse_event(MOUSEEVENTF_MOVE, -key->value, 0, 0, 0);
+void moveUp(const NavigationMapping *key) {
+  mouse_event(MOUSEEVENTF_MOVE, 0, -getValue(key), 0, 0);
 }
 
-void moveDown(NavigationMapping *key) {
-  mouse_event(MOUSEEVENTF_MOVE, 0, key->value, 0, 0);
+void moveLeft(const NavigationMapping *key) {
+  mouse_event(MOUSEEVENTF_MOVE, -getValue(key), 0, 0, 0);
 }
 
-void moveRight(NavigationMapping *key) {
-  mouse_event(MOUSEEVENTF_MOVE, key->value, 0, 0, 0);
+void moveDown(const NavigationMapping *key) {
+  mouse_event(MOUSEEVENTF_MOVE, 0, getValue(key), 0, 0);
 }
 
-void scrollUp(NavigationMapping *key) {
-  mouse_event(MOUSEEVENTF_WHEEL, 0, 0, WHEEL_DELTA * key->value, 0);
+void moveRight(const NavigationMapping *key) {
+  mouse_event(MOUSEEVENTF_MOVE, getValue(key), 0, 0, 0);
 }
 
-void scrollDown(NavigationMapping *key) {
-  mouse_event(MOUSEEVENTF_WHEEL, 0, 0, -WHEEL_DELTA * key->value, 0);
+void scrollUp(const NavigationMapping *key) {
+  mouse_event(MOUSEEVENTF_WHEEL, 0, 0, WHEEL_DELTA * getValue(key), 0);
 }
 
-void pressLeft(NavigationMapping *key) {
+void scrollDown(const NavigationMapping *key) {
+  mouse_event(MOUSEEVENTF_WHEEL, 0, 0, -WHEEL_DELTA * getValue(key), 0);
+}
+
+void pressLeft(const NavigationMapping *key) {
   mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
 }
 
-void releaseLeft(NavigationMapping *key) {
+void releaseLeft(const NavigationMapping *key) {
   mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
 }
 
-void pressRight(NavigationMapping *key) {
+void pressRight(const NavigationMapping *key) {
   mouse_event(MOUSEEVENTF_RIGHTDOWN, 0, 0, 0, 0);
 }
 
-void releaseRight(NavigationMapping *key) {
+void releaseRight(const NavigationMapping *key) {
   mouse_event(MOUSEEVENTF_RIGHTUP, 0, 0, 0, 0);
 }
 
-NavigationMapping rightPress = { 37,  0,   0, pressRight, releaseRight, NULL, NULL };
-NavigationMapping leftPress  = { 36,  0,   0, pressLeft, releaseLeft, NULL, &rightPress };
-NavigationMapping downScroll = { 24, 33,   0, scrollDown, NULL, NULL, &leftPress };
-NavigationMapping upScroll   = { 23, 33,   0, scrollUp, NULL, NULL, &downScroll };
-NavigationMapping rightFast  = { 34, 33, 100, moveRight, NULL, NULL, &upScroll };
-NavigationMapping downFast   = { 19, 33, 100, moveDown, NULL, NULL, &rightFast };
-NavigationMapping leftFast   = { 30, 33, 100, moveLeft, NULL, NULL, &downFast };
-NavigationMapping upFast     = { 17, 33, 100, moveUp, NULL, NULL, &leftFast };
-NavigationMapping rightSlow  = { 33, 33,  25, moveRight, NULL, NULL, &upFast };
-NavigationMapping downSlow   = { 32, 33,  25, moveDown, NULL, NULL, &rightSlow };
-NavigationMapping leftSlow   = { 31, 33,  25, moveLeft, NULL, NULL, &downSlow };
-NavigationMapping upSlow     = { 18, 33,  25, moveUp, NULL, NULL, &leftSlow };
+NavigationMapping rightPress = { 37,  0,   0,   0, pressRight, releaseRight, NULL, NULL };
+NavigationMapping leftPress  = { 36,  0,   0,   0, pressLeft, releaseLeft, NULL, &rightPress };
+NavigationMapping downScroll = { 24, 33,   1,   1, scrollDown, NULL, NULL, &leftPress };
+NavigationMapping upScroll   = { 23, 33,   1,   1, scrollUp, NULL, NULL, &downScroll };
+NavigationMapping rightFast  = { 34, 33, 100, 100, moveRight, NULL, NULL, &upScroll };
+NavigationMapping downFast   = { 19, 33, 100, 100, moveDown, NULL, NULL, &rightFast };
+NavigationMapping leftFast   = { 30, 33, 100, 100, moveLeft, NULL, NULL, &downFast };
+NavigationMapping upFast     = { 17, 33, 100, 100, moveUp, NULL, NULL, &leftFast };
+NavigationMapping rightSlow  = { 33, 33,  25,   4, moveRight, NULL, NULL, &upFast };
+NavigationMapping downSlow   = { 32, 33,  25,   4, moveDown, NULL, NULL, &rightSlow };
+NavigationMapping leftSlow   = { 31, 33,  25,   4, moveLeft, NULL, NULL, &downSlow };
+NavigationMapping upSlow     = { 18, 33,  25,   4, moveUp, NULL, NULL, &leftSlow };
 
 NavigationMapping *mapping = &upSlow;
 
@@ -89,16 +96,13 @@ void resetMouseNavigationState() {
 }
 
 bool navigateMouse(KBDLLHOOKSTRUCT keyInfo) {
-  bool isKeyUp = keyInfo.flags & LLKHF_UP;
-  int slow = (modStates.shift.leftIsPressed || modStates.shift.rightIsPressed) ? 4 : 25;
-
   NavigationMapping *curr = NULL;
 	for (curr = mapping; curr && curr->scan != keyInfo.scanCode; curr = curr->n);
 
   // ignore if we don't have a mapping for this key
   if (curr == NULL) return false;
 
-  if (!isKeyUp) {
+  if (!(keyInfo.flags & LLKHF_UP)) {
     createTimerIfndef(curr);
   } else {
     deleteTimerIfdef(curr);
